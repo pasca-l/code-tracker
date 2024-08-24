77bb24d24479728834e7f11944e7605ec9fbfcd2
pasca-l <shion.yamadate@keio.jp>
2024-08-24 21:56:31 +0900

    minor update

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,26 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+// 'vscode' module contains the VS Code extensibility API
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Method called when extension is activated,
+// which is the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // console.log() will show on "DEBUG CONSOLE"
+  console.log('"code-tracker" is active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-tracker" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    // The commandId parameter must match the command field in package.json
+    "code-tracker.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from code-tracker!");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-tracker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-tracker!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+// Method called when extension is deactivated
 export function deactivate() {}
